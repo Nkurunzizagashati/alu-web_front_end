5ac70b47a64c982b47d2d1ba9b13cac7dcbaaae1
Fabrice <nkurunziz@alustudent.com>
2024-03-15 10:57:42 +0200

    prime numbers and timing execution

---
+++ Javascript_advanced/10-prime.js
@@ -0,0 +1,31 @@
+function countPrimeNumbers() {
+  let limit = 100;
+  const arr = [];
+
+  for (let i = 2; i <= limit; i++) {
+    let j = 2;
+    for (j; j < i; j++) {
+      if (i % j === 0) {
+        j = 2;
+        break;
+      }
+    }
+
+    if (i === j) {
+      arr.push(i);
+    }
+  }
+
+  return arr;
+}
+const startTime = performance.now();
+for (let i = 0; i < 100; i++) {
+  countPrimeNumbers();
+}
+const endTime = performance.now();
+
+console.log(
+  `Execution time of calculating prime numbers 100 times was ${
+    endTime - startTime
+  } milliseconds.`
+);
