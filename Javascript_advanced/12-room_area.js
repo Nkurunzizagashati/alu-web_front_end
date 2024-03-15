35f3e8ce81b056c52d8c3d50c2881eeb1abfdd71
Fabrice <nkurunziz@alustudent.com>
2024-03-15 14:38:37 +0200

    binding

---
+++ Javascript_advanced/12-room_area.js
@@ -0,0 +1,9 @@
+const roomDimensions = {
+  with: 50,
+  length: 100,
+  getArea: function () {
+    return this.width * this.length;
+  },
+};
+
+const boundGetArea = roomDimensions.getArea.bind(roomDimensions);
